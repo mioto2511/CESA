ef35f52bbc6e6989f5daf04325d2e3672ffc8ac8
hagi <1343.hagi@gmail.com>
2022-02-12 00:44:56 +0900

    no message

---
+++ Assets/Script/Camera/MoveCamera.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    Transform tf;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //カメラのTransformを取得
+        tf = this.gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rsh = Input.GetAxis("R_Stick_H");//横軸
+        float rsv = Input.GetAxis("R_Stick_V");//縦軸
+
+        if (rsh < 0)
+        {
+            tf.position = tf.position + new Vector3(-speed, 0.0f, 0.0f); //カメラを左へ移動。
+        }
+        else if (rsh > 0)
+        {
+            tf.position = tf.position + new Vector3(+speed, 0.0f, 0.0f); //カメラを左へ移動。
+        }
+
+
+        if (rsv > 0) //Iキーが押されていれば
+        {
+            tf.position = tf.position + new Vector3(0.0f, +speed, 0.0f); //カメラを左へ移動。
+        }
+        else if (rsv < 0) //Iキーが押されていれば
+        {
+            tf.position = tf.position + new Vector3(0.0f, -speed, 0.0f); //カメラを左へ移動。
+        }
+    }
+}
