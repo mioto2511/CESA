3e59cd12b89eed0113973795bddb751087ababee
hagi <1343.hagi@gmail.com>
2022-02-15 01:04:27 +0900

    EnterをStayに

--- Assets/Script/Gear/Placement/SelectCollision.cs
+++ Assets/Script/Gear/Placement/SelectCollision.cs
@@ -7,7 +7,7 @@ public class SelectCollision : MonoBehaviour
     public bool cursorhit;
 
     //•Ô‚ÉG‚ê‚½‚ç
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Gear"))
         {
