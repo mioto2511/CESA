a2ff45e025439240923f1cb8219c518c4bb3faaf
mioto2511 <syun.17.09@gmail.com>
2022-03-14 19:18:52 +0900

    部屋の接続時の壁の除去

---
+++ Assets/Script/Gear/connection_gear/conntion_Gearconnect.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class conntion_Gearconnect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    //Ú‘±•Ô‚ÆÚ‘±‚µ‚½ó‘Ô
+    void OnTriggerStay2D(Collider2D other)
+    {
+        //•Ô‚É“–‚½‚Á‚½
+        if (other.gameObject.CompareTag("RGear"))
+        {
+            // ‰ñ“]ƒtƒ‰ƒOƒIƒ“
+            other.tag = "RGear_Connect";
+            Debug.Log("a");
+        }
+        else if (other.gameObject.CompareTag("LGear"))
+        {
+            other.tag = "LGear_Connect";
+            Debug.Log("connect");
+        }
+       
+
+    }
+}
