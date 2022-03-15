cdcbc1b015941a0156c247e8f6a8ffcf08743a8b
kazalpa <kazalpa_qs@icloud.com>
2022-03-15 10:31:48 +0900

    canera

--- Assets/Script/Camera/MoveCamera.cs
+++ Assets/Script/Camera/MoveCamera.cs
@@ -8,36 +8,91 @@ public class MoveCamera : MonoBehaviour
 
     public float speed;
 
+    private Camera cam;
+    public GameObject room;
+    private Vector2 pos;
+    private GameObject parent;
+    public int camerazoom = 1;
+    private bool cflg = false;
+
     // Start is called before the first frame update
     void Start()
     {
         //カメラのTransformを取得
         tf = this.gameObject.GetComponent<Transform>();
+
+        cam = this.gameObject.GetComponent<Camera>();//Camera取得
     }
 
     // Update is called once per frame
     void Update()
     {
-        float rsh = Input.GetAxis("R_Stick_H");//横軸
-        float rsv = Input.GetAxis("R_Stick_V");//縦軸
+        //float rsh = Input.GetAxis("R_Stick_H");//横軸
+        //float rsv = Input.GetAxis("R_Stick_V");//縦軸
 
-        if (rsh < 0)
-        {
-            tf.position = tf.position + new Vector3(-speed, 0.0f, 0.0f);
-        }
-        else if (rsh > 0)
-        {
-            tf.position = tf.position + new Vector3(+speed, 0.0f, 0.0f);
-        }
+        //if (rsh < 0)
+        //{
+        //    tf.position = tf.position + new Vector3(-speed, 0.0f, 0.0f);
+        //}
+        //else if (rsh > 0)
+        //{
+        //    tf.position = tf.position + new Vector3(+speed, 0.0f, 0.0f);
+        //}
 
 
-        if (rsv > 0) //Iキーが押されていれば
+        //if (rsv > 0) //Iキーが押されていれば
+        //{
+        //    tf.position = tf.position + new Vector3(0.0f, +speed, 0.0f);
+        //}
+        //else if (rsv < 0) //Iキーが押されていれば
+        //{
+        //    tf.position = tf.position + new Vector3(0.0f, -speed, 0.0f);
+        //}
+
+        pos = room.transform.position;
+        transform.position = new Vector3(pos.x, pos.y, -10);
+
+        if (Input.GetKeyDown("joystick button 2"))
         {
-            tf.position = tf.position + new Vector3(0.0f, +speed, 0.0f);
+            if (cflg == false)
+            {
+                cflg = true;
+            }
+            else if (cflg == true)
+            {
+                cflg = false;
+            }
         }
-        else if (rsv < 0) //Iキーが押されていれば
+
+        switch (GameObject.Find("Room").transform.childCount)
         {
-            tf.position = tf.position + new Vector3(0.0f, -speed, 0.0f);
+            case 2:
+                camerazoom = 2;
+                break;
+
+            case 3:
+                camerazoom = 3;
+                break;
+        }
+
+        if (cflg == true)
+        {
+            if (camerazoom == 1)
+            {
+                cam.orthographicSize = 4.2f;
+            }
+            if (camerazoom == 2)
+            {
+                cam.orthographicSize = 10.2f;
+            }
+            if (camerazoom == 3)
+            {
+                cam.orthographicSize = 16.2f;
+            }
+        }
+        if (cflg == false)
+        {
+            cam.orthographicSize = 16.2f;
         }
     }
 }
