ac282b6ed54a3a841674f9720b55f281e4d57690
yuki\yukig <kumananonano.0624@gmail.com>
2022-03-17 08:44:46 +0900

    タイトルとフェード追加

---
+++ Assets/Scenes/Scene_Management/Change_Scene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Change_Scene : MonoBehaviour
+{
+    private float step_time;    // 経過時間カウント用
+
+    // Use this for initialization
+    void Start()
+    {
+        step_time = 0.0f;   // 経過時間初期化
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Shutter"))
+        {
+            Fade_Manager.FadeOut(2); // フェードイン開始、番号でフェード後のsceneを指定
+        }
+       
+    }
+}
---
+++ Assets/Scenes/Title_Shutter/ShutterR.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShutterR : MonoBehaviour
+{
+    private Vector2 pos;
+    bool Change_Start = false;
+
+    private void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Transform mytransform = this.transform;
+        pos = mytransform.position;
+
+        //Aボタンが押されたら開始
+        if (Input.GetKeyDown("joystick button 0"))
+        {
+            Change_Start = true;
+        }
+
+        if (Change_Start == true)
+        {
+            
+            if ((pos.x >= 4.5f))
+            {
+                //Debug.Log("R");
+                pos.x -= 0.01f;
+                transform.position = new Vector2(pos.x, pos.y);
+            }
+        }
+
+    }
+}
