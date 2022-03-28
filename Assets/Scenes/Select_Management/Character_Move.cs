c71d82953c49877138fc0f75b7e735b6668bb2f8
yuki\yukig <kumananonano.0624@gmail.com>
2022-03-28 19:32:24 +0900

    各ポイントへ移動(完成)

--- Assets/Scenes/Select_Management/Character_Move.cs
+++ Assets/Scenes/Select_Management/Character_Move.cs
@@ -7,14 +7,19 @@ public class Character_Move : MonoBehaviour
     [Header("移動経路")] public GameObject[] movePoint;
     [Header("早さ")] public float speed = 1.0f;
 
+
     private Rigidbody2D rb = null;
     private int nowPoint = 0;
-    private bool returnPoint = false;
+    private bool PointMax = false;
+    private bool PointMin = false;
+    private bool RightMove = false;
+    private bool LeftMove = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         if (movePoint != null && movePoint.Length > 0 && rb != null)
         {
             //キャラクターを移動経路の０番目の位置へ
@@ -25,57 +30,84 @@ public class Character_Move : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        // 通常移動
-        if (!returnPoint)
-        {
-            int nextPoint = nowPoint + 1;
-
-            // 目標のポイントまでの距離が僅かになるまで移動
-            if (Vector2.Distance(transform.position, movePoint[nextPoint].transform.position) > 0.1f)
-            {
-                // 現在地から次のポイントへのベクトルを計算
-                Vector2 toVector = Vector2.MoveTowards(transform.position, movePoint[nextPoint].transform.position, speed * Time.deltaTime);
-                
-                // 次のポイントへ移動
-                rb.MovePosition(toVector);
-            }
-            else
-            { //次のポイントを１つ進める 
-                rb.MovePosition(movePoint[nextPoint].transform.position);
-                ++nowPoint;
-
-                // 現在地が配列の最後だった場合
-                if (nowPoint + 1 >= movePoint.Length)
-                {
-                    returnPoint = true;
-                }
-            }
-        }
-        else
-        {
-            int nextPoint = nowPoint + 1;
-
-            // 目標のポイントまでの距離が僅かになるまで移動
-            if (Vector2.Distance(transform.position, movePoint[nextPoint].transform.position) > 0.1f)
-            {
-                // 現在地から次のポイントへのベクトルを計算
-                Vector2 toVector = Vector2.MoveTowards(transform.position, movePoint[nextPoint].transform.position, speed * Time.deltaTime);
-
-                // 次のポイントへ移動
-                rb.MovePosition(toVector);
-            }
-            else
-            { // 次のポイントを１つ進める 
-                rb.MovePosition(movePoint[nextPoint].transform.position);
-                --nowPoint;
-
-                // 現在地が配列の最初だった場合
-                if (nowPoint <=0)
-                {
-                    returnPoint = false;
-                }
-            }
-        }
         
+        if (Input.GetKey(KeyCode.RightArrow) && RightMove == false && LeftMove == false && PointMax == false)
+        {
+            RightMove = true;
+            Debug.Log("R");
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && RightMove == false && LeftMove == false && PointMin == false)
+        {
+            LeftMove = true;
+            Debug.Log("L");
+        }
+
+        if (movePoint != null && movePoint.Length > 1 && rb != null)
+        {
+            // 通常進行
+            if (RightMove == true)
+            {
+                int nextPoint = nowPoint + 1;
+
+                // 目標のポイントまでの距離が僅かになるまで移動
+                if (Vector2.Distance(transform.position, movePoint[nextPoint].transform.position) > 0.1f)
+                {
+                    // 現在地から次のポイントへのベクトルを計算
+                    Vector2 toVector = Vector2.MoveTowards(transform.position, movePoint[nextPoint].transform.position, speed * Time.deltaTime);
+
+                    // 次のポイントへ移動
+                    rb.MovePosition(toVector);
+                }
+                else
+                { // 次のポイントを１つ進める 
+                    rb.MovePosition(movePoint[nextPoint].transform.position);
+                    ++nowPoint;
+                    RightMove = false;
+
+                    
+                }
+            }
+            else if (LeftMove == true)
+            {
+                int nextPoint = nowPoint - 1;
+
+                // 目標のポイントまでの距離が僅かになるまで移動
+                if (Vector2.Distance(transform.position, movePoint[nextPoint].transform.position) > 0.1f)
+                {
+                    // 現在地から次のポイントへのベクトルを計算
+                    Vector2 toVector = Vector2.MoveTowards(transform.position, movePoint[nextPoint].transform.position, speed * Time.deltaTime);
+
+                    // 次のポイントへ移動
+                    rb.MovePosition(toVector);
+                }
+                else
+                { // 次のポイントを１つ進める 
+                    rb.MovePosition(movePoint[nextPoint].transform.position);
+                    --nowPoint;
+                    LeftMove = false;
+                }
+            }
+            // 現在地が配列の最後だった場合
+            if (nowPoint + 1 >= movePoint.Length)
+            {
+                PointMax = true;
+                Debug.Log("max");
+            }
+            else
+            {
+                PointMax = false;
+            }
+            // 現在地が配列の最初だった場合
+            if (nowPoint <= 0)
+            {
+                PointMin = true;
+                Debug.Log("min");
+            }
+            else
+            {
+                PointMin = false;
+            }
+        }
+
     }
 }
