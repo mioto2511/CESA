7d46f14613b26b116827eb21e50c7ffbce66984a
kazalpa <kazalpa_qs@icloud.com>
2022-05-02 02:01:11 +0900

    effect

---
+++ Assets/Script/EffectGear/GEffect.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GEffect : MonoBehaviour
+{
+    public GameObject effectObject;
+
+    private bool isEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isEffect = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (isEffect)
+        {
+            if (other.CompareTag("RGear_Connect") || other.CompareTag("LGear_Connect"))
+            {
+                Debug.Log("E");
+                Instantiate(effectObject, this.transform.position, Quaternion.identity);
+                isEffect = false;
+            }
+        }
+    }
+}
